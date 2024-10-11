d006d5b69eb0431169bfd2bdfdb6b30996c6a7c1
rschellenberg <ryan@theschellenbergs.com>
2024-10-11 11:20:01 -0400

    updated to modules

--- main.tf
+++ main.tf
@@ -1,10 +1,19 @@
 terraform {
   required_providers {
-    aws = { source = "hashicorp/aws", version = "5.17.0" }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.17"
+    }
   }
+
+  required_version = ">= 1.2.0"
 }
 
 provider "aws" {
-  profile = "default"
   region  = "ca-central-1"
+}
+
+module "networking" {
+	source  = "./networking"
+	#placeholder for vars
 }
